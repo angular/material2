76beab48c97440f7336bf2204f173b24a2744b42
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2021-05-24 12:22:51 -0400

    feat(multiple): add options to autoFocus field for dialogs
    Before this PR, autoFocus was a boolean that allowed users to specify whether the container element or the first tabbable element is focused on dialog open. Now you can also specify focusing the first header element or use a CSS selector and focus the first element that matches that. If these elements can't be focused, then the container element is focused by default.
    
    This applies to other components that are similar to dialog and also have a autoFocus field.
    
    Fixes #22678

--- src/material/sidenav/drawer.spec.ts
+++ src/material/sidenav/drawer.spec.ts
@@ -580,8 +580,9 @@ describe('MatDrawer', () => {
       expect(document.activeElement).toBe(lastFocusableElement);
     }));
 
-    it('should auto-focus when opened in "side" mode when enabled explicitly', fakeAsync(() => {
-      drawer.autoFocus = true;
+    it('should auto-focus to first tabbable element when opened in "side" mode' +
+      'when enabled explicitly', fakeAsync(() => {
+      drawer.autoFocus = 'first-tabbable';
       testComponent.mode = 'side';
       fixture.detectChanges();
       lastFocusableElement.focus();
@@ -608,7 +609,7 @@ describe('MatDrawer', () => {
     }));
 
     it('should be able to disable auto focus', fakeAsync(() => {
-      drawer.autoFocus = false;
+      drawer.autoFocus = 'dialog';
       testComponent.mode = 'push';
       fixture.detectChanges();
       lastFocusableElement.focus();
